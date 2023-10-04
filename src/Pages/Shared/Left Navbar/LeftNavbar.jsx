4a851b0929e99fcec9c820f482f50f09d727ae84
webdevmahdi <mahdihasan01705259@gmail.com>
2023-10-04 21:58:35 +0600

    left navbar setup

--- src/Pages/Shared/Left Navbar/LeftNavbar.jsx
+++ src/Pages/Shared/Left Navbar/LeftNavbar.jsx
@@ -2,8 +2,25 @@ import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom';
 
 const LeftNavbar = () => {
+  let [catagories, setCatagories] = useState([])
+  
+  useEffect( ()=>{
+    fetch('http://localhost:5000/catagories')
+    .then(res => res.json())
+    .then(data => setCatagories(data))
+    .catch(err => console.log(err));
+  } ,[])
+  console.log(catagories)
   return (
     <div>
+      <h3>All catagories</h3>
+      <div className='p-4'>
+        {
+          catagories.map(category => <p key={category.id}>
+            <Link className='d-flex align-items-start text-decoration-none text-black fs-5 p-3 border-rounded bg-secondary' to={`/category/${category.id}`}>{category.name}</Link>
+          </p>)
+        }
+      </div>
     </div>
   )
 }
